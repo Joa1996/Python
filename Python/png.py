709029c527be4b62a9089f2871bf0cfaeabee9df
Joaquin Gallardo <67919022+Joa1996@users.noreply.github.com>
2021-02-23 12:32:38 -0300

    png
    Como pasar una imagen  de PNG a JPG con python

--- Python/png.py
+++ Python/png.py
@@ -1,3 +1,3 @@
 from PIL import Image
 ruta_png=Image.open('D:/Practicas/ETL_Python/sami.png')
-ruta_png.save('D:/Practicas/ETL_Python/sami2.jpg')
+ruta_png.save('D:/Practicass/ETL_Python/sami2.jpg')
