c57daec90f24363618f7136c2c81bbbe9c2cff1f
Joaquin Gallardo <67919022+Joa1996@users.noreply.github.com>
2021-05-15 01:53:45 -0300

    Add files via upload

---
+++ ETL/ConectarSQLserver.py
@@ -0,0 +1,10 @@
+import pyodbc as pb
+conexion = pb.connect('Driver={SQL Server};'
+                      'Server=Nombre Servidor;'
+                      'Database=Nombre BD;'
+                      'Trusted_Connection=yes;')
+cursor = conexion.cursor()
+cursor.execute('SELECT * FROM Destino.dbo.Compras') #Ejecutamos una query
+for row in cursor:
+    print(row)
+
