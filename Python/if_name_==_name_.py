68d6b7bbabcf706a32578d4831f6543a63d3ad9d
Joaquin Gallardo <67919022+Joa1996@users.noreply.github.com>
2022-05-10 20:42:59 -0300

    Update if_name_==_name_.py

--- Python/if_name_==_name_.py
+++ Python/if_name_==_name_.py
@@ -2,9 +2,12 @@
 main representa el nombre del modulo que el interprete de python esta ejecutando
 entonces al hacer  if __name__ == '__main__' le estamos preguntando a python si esta ejecutando actualmente
 este modulo o es otro importado.
-Esto es util por ejemplo si queremos que los metodos de un modulo solo se ejecuten solamente cuando ese
-modulo se esta ejecutando, es decir si otro modulo importa a este, que este que lo importa
-no pueda ejecutar sus metodos
+Esto es util para los Scripts, ya que es una forma de saber que ese modulo de python contiene el Script. 
+Ademas permite tener el control de todo el modulo desde un solo lugar, tambien se recomienda en caso de que el Modulo tenga 
+muchos metodos, Usar un metodo Main que llame a todos los metodos de ese modulo
+def main():
+ saludoinicial()
+ saludofinal()
 '''
 
 print("File __name__ : {}".format(__name__)) #imprimira name
